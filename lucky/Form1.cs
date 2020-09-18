3ad61d226c7ac9d26e21b36e6bf1ef0dadc35baa
Latha Kumarasamy Sakthivel <lathasowdi@gmail.com>
2020-09-18 08:40:23 +0200

    Add project files.

---
+++ lucky/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lucky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       static List<string> Naamen = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+           Naamen.Add("Aarthi");
+            Naamen.Add("Cat");
+            Naamen.Add("Mat");
+            Naamen.Add("Hat");
+            Naamen.Add("Kalyan");
+            listBox1.DataSource = Naamen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.DataSource = Shuffle(Naamen);
+        }
+            
+        public static List<String> Shuffle(List<String> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                string value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return list;
+        }
+
+    }
+
+}
---
+++ lucky/Vegetables.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lucky
+{
+    public class Vegetables
+    {
+        public string Naam { get; set; }
+        public Vegetables(string naam)
+        {
+            Naam = naam;
+        }
+        public override string ToString()
+        {
+            return Naam;
+        }
+        
+    }
+
+}
+
